038330ff0564e7681e6672636b3a79b9d3db941e
Datalcoholic <rechezuria@gmail.com>
2019-10-15 17:41:19 -0400

    init commit

---
+++ an/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Barlow+Condensed:400,600,900&display=swap");
+#app {
+  font-family: "Barlow Condensed", Helvetica, Arial, sans-serif;
+}
+</style>
